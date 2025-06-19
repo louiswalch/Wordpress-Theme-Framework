50a8f85f66afad84cafa308353bc00a780f7e102
Louis Walch <me@louiswalch.com>
2025-06-19 10:21:29 -0400

    Helper function for outputting an html element

--- public/string.php
+++ public/string.php
@@ -36,5 +36,41 @@ namespace {
         }
     }
 
+    /*
+
+        echo element('div', [ 'class'=>'hello' ], 'Hello World');
+        echo element('div', 'Hello World', [ 'class'=>'hello' ]);
+        echo element('h1', get_the_title());
+
+    */
+    function str_element($tag, $param2=null, $param3=null) {
+        
+        $attributes     = [];
+        $content        = '';
+
+        if (is_array($param2)) {
+            $attributes = $param2;
+            $content    = (string) $param3;
+        } elseif ( is_string($param2) || is_numeric($param2)) {
+            $content    = (string) $param2;
+            if (is_array($param3)) {
+                $attributes = $param3;
+            }
+        }
+
+        $attributes_str = '';
+        if (!empty($attributes)) {
+            $attributes_str = ' ' . implode(' ', array_map( fn($k, $v) => sprintf('%s="%s"', esc_attr($k), esc_attr($v)), array_keys($attributes), $attributes) );
+        }
+
+        return sprintf('<%1$s%2$s>%3$s</%1$s>', esc_html($tag), $attributes_str, esc_html($content));
+
+    }
+    if (!function_exists('element')) {
+        function element($tag, $param2=null, $param3=null) {
+            return str_element($tag, $param2, $param3);
+        }
+    }
+         
 
 }
